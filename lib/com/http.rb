c3a37ca15b15c3f14fcd4df4d18dc78ffc8c3d80
Zeke Fast <zekefast@gmail.com>
2016-09-13 21:02:06 +0200

    Remove accidentally committed pieces of code.

--- lib/com/http.rb
+++ lib/com/http.rb
@@ -2,18 +2,7 @@
 
 # Purpose: namespace
 module ::Com::Http
-  # @return [::Com::Http::AbstractFactory]
-  def build_abstract_factory
-    ::Com::Http::AbstractFactory.new
-  end
 end
 
 require "com/http/error"
 require "com/http/standard_error"
-
-require "com/http/query_serializer"
-require "com/http/url"
-require "com/http/request"
-require "com/http/request_factory"
-require "com/http/default_client"
-require "com/http/abstract_factory"
